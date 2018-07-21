e37d00e379fca4441c2bfec38513e7ecf4f579d0
Yaiphabee <ksh.yaipha@gmail.com>
2018-07-21 13:35:10 +0530

    Added isogram after build

--- Isogram.Tests/UnitTest1.cs
+++ Isogram.Tests/UnitTest1.cs
@@ -6,9 +6,51 @@ namespace Isogram.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void emptyString()
         {
+            Assert.True(Class1.IsIsogram(""));
+        }
 
+        [Fact]
+        public void isogram()
+        {
+            Assert.True(Class1.IsIsogram("isogram"));
+        }
+
+        [Fact]
+        public void eleven()
+        {
+            Assert.False(Class1.IsIsogram("eleven"));
+        }
+
+        [Fact]
+        public void longWord()
+        {
+            Assert.True(Class1.IsIsogram("subdermatoglyphic"));
+        }
+
+        [Fact]
+        public void upperAndLower()
+        {
+            Assert.True(Class1.IsIsogram("Alphabet"));
+        }
+
+        [Fact]
+        public void thumbScrew()
+        {
+            Assert.True(Class1.IsIsogram("thumbscrew-japingly"));
+        }
+
+        [Fact]
+        public void hyphen()
+        {
+            Assert.True(Class1.IsIsogram("six-year-old"));
+        }
+
+        [Fact]
+        public void spaceName()
+        {
+            Assert.True(Class1.IsIsogram("Angit Devy"));
         }
     }
 }
